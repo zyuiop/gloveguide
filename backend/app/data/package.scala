587c4160340ffaa0571f049f6b9ff958ce1cac4f
Louis Vialar <louis.vialar@gmail.com>
2020-05-12 18:06:54 +0200

    Add copyright headers on scala files

--- backend/app/data/package.scala
+++ backend/app/data/package.scala
@@ -4,6 +4,24 @@ import anorm._
 import anorm.SqlParser._
 import anorm.Macro.ColumnNaming
 import anorm.{Column, Macro, RowParser, SqlParser, ToParameterList, ToStatement}
+/*
+ *     EPFL Gloves Guide - An application to help you choose the best gloves for a chemical work
+ *     Copyright (C) 2020 - Louis Vialar & EPFL GSCP
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Affero General Public License as published
+ *     by the Free Software Foundation, either version 3 of the License, or
+ *     any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Affero General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Affero General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 import play.api.libs.json.{Format, Json, OFormat}
 
 package object data {
